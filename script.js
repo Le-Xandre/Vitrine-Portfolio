e51e4d0fb7cd62690068881d00febeae67c88c8d
Alx <112096089+Le-Xandre@users.noreply.github.com>
2025-04-21 18:41:32 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,40 @@
+let btnContainer = document.querySelector('.btn-container');
+let button = document.querySelector('.glow-btn');
+
+let spawnDistance = 20;
+
+function createParticles() {
+    let particles = document.createElement('div');
+    particles.classList.add('particles');
+    let btnWidth = button.offsetWidth;
+    let btnHeight = button.offsetHeight;
+
+    let angle = Math.random() * 2 * Math.PI;
+    let x = btnWidth / 2 + Math.cos(angle) * spawnDistance;
+    let y = btnHeight / 2 + Math.sin(angle) * spawnDistance;
+
+    let dx = Math.cos(angle) * 100;
+    let dy = Math.sin(angle) * 100;
+
+    particles.style.left = `${x}px`;
+    particles.style.top = `${y}px`;
+    particles.style.setProperty('--dx', `${dx}px`);
+    particles.style.setProperty('--dy', `${dy}px`);
+
+    btnContainer.appendChild(particles);
+    setTimeout(() => {
+        particles.remove();
+    }, 2000);
+}
+
+button.addEventListener('mouseenter', () => {
+    interval = setInterval(createParticles, 80);
+});
+
+button.addEventListener('mouseleave', () => {
+    clearInterval(interval);
+});
+
 document.addEventListener("DOMContentLoaded", () => {
     const themeToggleButton = document.getElementById("theme-toggle");
     const body = document.body;
